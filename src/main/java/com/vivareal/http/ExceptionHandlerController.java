273a2658c13777655971cd8156809e375990f467
Fernando Pillo <fernandopillo@gmail.com>
2017-03-24 22:13:52 -0300

    added handler controller

---
+++ src/main/java/com/vivareal/http/ExceptionHandlerController.java
@@ -0,0 +1,27 @@
+package com.vivareal.http;
+
+import com.vivareal.exceptions.ResourceAlreadyExistsException;
+import com.vivareal.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by fpillo on 3/24/2017.
+ */
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleResourceNotFoundException(final ResourceNotFoundException ex) {
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public void handleResourceAlreadyExistsException(final ResourceAlreadyExistsException ex) {
+    }
+
+
+}
